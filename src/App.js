33ce18041b4a3a20bc0f683e91166df4566a584f
Donald Brooks <brooksd079@gmail.com>
2023-06-25 17:25:41 +0300

    React router dom added, links to the products page updated using react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 import './App.css';
 
 function App() {
   return (
-    <div >
-      <Header/>
-      <main className='py-3'>
+    <Router>
+      <Header />
+      <main className="py-3">
         <Container>
           {/* add ref for whoever is logged in */}
           <h4>Welcome Brooks</h4>
-          <HomeScreen/>
+            <Route exact path="/" component={HomeScreen} />
+            <Route path="/product/:id" component={ProductScreen}/>
         </Container>
       </main>
-      <Footer/>
-    </div>
+      <Footer />
+    </Router>
   );
 }
 
