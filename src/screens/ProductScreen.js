6ef5a6d2871f07769be129fe9644a923cfab6c5d
Donald Brooks <brooksd079@gmail.com>
2023-06-25 18:04:26 +0300

    Product screen functionality initial implentation done

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card} from 'react-router-bootstrap'
+import { Row, Col, Image, ListGroup, Button, Card} from 'react-bootstrap'
 import Rating from '../components/Rating'
 import products from '../products'
 
@@ -8,9 +8,12 @@ function ProductScreen({ match }) {
     const product = products.find((p) => p._id === match.params.id)
     return (
     <div>
-        <Link to='/'> Go Back </Link>
-        {product.name}
-        <Rating/>
+        <Link to='/' className='btn btn-light my-3'> Go Back </Link>
+        <Row>
+            <Col md={6}> 
+                <Image src={product.image} alt={product.name} fluid/>
+            </Col>
+        </Row>
     </div>
   )
 }
