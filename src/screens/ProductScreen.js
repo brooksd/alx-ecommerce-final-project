c170e6195a632df3389ce5536b6391d803cefb36
Donald Brooks <brooksd079@gmail.com>
2023-06-25 17:53:40 +0300

    Product screen test validated showing results

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card} from 'react-router-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-function ProductScreen() {
-  return (
-    <div>ProductScreen</div>
+function ProductScreen({ match }) {
+    const product = products.find((p) => p._id === match.params.id)
+    return (
+    <div>
+        <Link to='/'> Go Back </Link>
+        {product.name}
+        <Rating/>
+    </div>
   )
 }
 
