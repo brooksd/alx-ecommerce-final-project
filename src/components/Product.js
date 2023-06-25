5a1489c931b50f2b05212830bd80209af4e76150
Donald Brooks <brooksd079@gmail.com>
2023-06-25 13:01:31 +0300

    product component card descriptions added

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,6 +8,19 @@ function Product({ product }) {
         <a href={`/product/${product._id}`}>
             <Card.Img src={product.image}/>
         </a>
+        <Card.Body>
+             <a href={`/product/${product._id}`}>
+                <Card.Title as='div'>
+                    <strong>{product.name}</strong>
+                </Card.Title>
+            </a>
+            <Card.Text as='div'>
+                <div className='my-3'>
+                    {/* implement a randomizer for the ratings */}
+                    {product.rating} from {product.numReviews} reviews
+                </div>
+            </Card.Text>
+        </Card.Body>
     </Card>
   )
 }
