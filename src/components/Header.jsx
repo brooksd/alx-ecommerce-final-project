0163bb987abf76f176fc7b720b7c58c2b83db4bc
Donald Brooks <brooksd079@gmail.com>
2023-07-13 23:12:15 +0300

    alx audio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
 		<Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
 			<Container>
 				<LinkContainer to="/">
-					<Navbar.Brand href="/">ProShop</Navbar.Brand>
+					<Navbar.Brand href="/">ALX Audio</Navbar.Brand>
 				</LinkContainer>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
